734652c2767cbad1df0c6c68d5700a8d7ab87fa9
yuramon <yuramon2405@gmail.com>
2019-05-12 22:00:07 +0300

    Without ->

---
+++ calculator/class/SquareCommand.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class SquareCommand implements OperatorInterface
+{
+
+    public function run($number, $result)
+    {
+        return pow($result, $number);
+    }
+}
--- calculator/index.php
+++ calculator/index.php
@@ -5,13 +5,17 @@ require_once 'class/SumCommand.php';
 require_once 'class/SubCommand.php';
 require_once 'class/MultCommand.php';
 require_once 'class/DivCommand.php';
+require_once 'class/SquareCommand.php';
 
 
 $calc = new Calculator();
 $calc->addCommand('+',new SumCommand());
 $calc->addCommand('-', new SubCommand());
+$calc->addCommand('^', new SquareCommand());
 $calc->init(1);
 $calc->compute('+', 5);
 $calc->compute('-', 3);
+$calc->compute('^',2);
+
 echo $calc->getResult();
 
